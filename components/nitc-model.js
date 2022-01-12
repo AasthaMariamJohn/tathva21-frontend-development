eba690e30cf31c45af42814831c559d61354e0e7
Amal Prakash <amalkj20000@gmail.com>
2022-01-12 07:11:39 +0300

    Click Functionality Added

--- components/nitc-model.js
+++ components/nitc-model.js
@@ -1,11 +1,10 @@
 import { AspectRatio } from '@chakra-ui/react'
 import { useState, useEffect, useRef, useCallback } from 'react'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { MapControls } from 'three/examples/jsm/controls/OrbitControls'
 import { degToRad } from 'three/src/math/MathUtils'
 import { loadGLTFModel } from '../lib/model'
-import { ModelSpinner, ModelContainer } from './nitc-model-loader'
+import { ModelContainer } from './nitc-model-loader'
 
 
 const Button3D = (name , scene, x, y, z) => {
@@ -18,7 +17,7 @@ const Button3D = (name , scene, x, y, z) => {
   mesh.position.set(x,y,z);
   mesh.name = name
   scene.add(mesh);   
-  //camera.lookAt(mesh.position);
+
   
   setInterval(() => {
     mesh.rotateY(degToRad(3));
@@ -34,7 +33,29 @@ function onMouseMove( event , mouse) {
 	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 
 }
+function onMouseDown(event, scene, camera, raycaster, mouse){
+  raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObjects(scene.children);
+    for(let i =0; i<intersects.length; i++){
+      if(intersects[i].object.name==='ECLCButton'){
+        console.log(intersects[i].object.name,' clicked')
 
+      }
+      else if(intersects[i].object.name==='OATButton'){
+        console.log(intersects[i].object.name,' clicked')
+     
+      }
+      else if(intersects[i].object.name==='MBButton'){
+        console.log(intersects[i].object.name,' clicked')
+     
+      }
+      else if(intersects[i].object.name==='CreativeButton'){
+        console.log(intersects[i].object.name,' clicked')
+     
+       }
+      
+    }
+}
 const NITCModel3D = () => {
   const refContainer = useRef()
   const [loading, setLoading] = useState(true)
@@ -121,6 +142,9 @@ const NITCModel3D = () => {
       window.addEventListener( 'mousemove', (event)=>{
         onMouseMove(event, mouse)
       }, false );
+      window.addEventListener( 'mousedown',(event)=>{
+        onMouseDown(event, scene, camera, raycaster, mouse)
+      } , false );
       setControls(controls)
 
       loadGLTFModel(scene, '/scene.glb', {
@@ -164,11 +188,13 @@ const NITCModel3D = () => {
       if(loading===false)
       {
         const eclc = scene.getObjectByName('ECLC')
-        console.log(loading)
-        console.log(eclc)
+        const oat002 = scene.getObjectByName('OAT002')
+        const mb = scene.getObjectByName('MB')
+        const creative = scene.getObjectByName('Creative')
         Button3D('ECLCButton',scene,eclc.position.x,eclc.position.y,eclc.position.z)
-        
-        //debugger
+        Button3D('OATButton',scene,oat002.position.x,oat002.position.y,oat002.position.z)
+        Button3D('MBButton',scene,mb.position.x,mb.position.y,mb.position.z)
+        Button3D('CreativeButton',scene,creative.position.x,creative.position.y,creative.position.z)
       }
     },[loading])
 
@@ -179,14 +205,63 @@ const NITCModel3D = () => {
     for(let i =0; i<intersects.length; i++){
       if(intersects[i].object.name==='ECLCButton'){
         intersects[i].object.material.color.setHex(0x0000ff)
-        console.log(intersects[i].object.name)
+        intersects[i].object.scale.x = 2
+        intersects[i].object.scale.y = 2
+        intersects[i].object.scale.z = 2
       }
+      else if(intersects[i].object.name==='OATButton'){
+        intersects[i].object.material.color.setHex(0x0000ff)
+        intersects[i].object.scale.x = 2
+        intersects[i].object.scale.y = 2
+        intersects[i].object.scale.z = 2
+     
+      }
+      else if(intersects[i].object.name==='MBButton'){
+        intersects[i].object.material.color.setHex(0x0000ff)
+        intersects[i].object.scale.x = 2
+        intersects[i].object.scale.y = 2
+        intersects[i].object.scale.z = 2
+     
+      }
+      else if(intersects[i].object.name==='CreativeButton'){
+        intersects[i].object.material.color.setHex(0x0000ff)
+        intersects[i].object.scale.x = 2
+        intersects[i].object.scale.y = 2
+        intersects[i].object.scale.z = 2
+     
+       }
+      
     }
   }
   const resetHover = (scene) =>{
     const eclcbutton = scene.getObjectByName('ECLCButton')
+    const oat002button = scene.getObjectByName('OATButton')
+    const mbbutton = scene.getObjectByName('MBButton')
+    const creativebutton = scene.getObjectByName('CreativeButton')
+
     if(eclcbutton){
       eclcbutton.material.color.setHex(0xff0000)
+      eclcbutton.scale.x = 1
+      eclcbutton.scale.y = 1
+      eclcbutton.scale.z = 1
+    }
+    if(oat002button){
+      oat002button.material.color.setHex(0xff0000)
+      oat002button.scale.x = 1
+      oat002button.scale.y = 1
+      oat002button.scale.z = 1
+    }
+    if(mbbutton){
+      mbbutton.material.color.setHex(0xff0000)
+      mbbutton.scale.x = 1
+      mbbutton.scale.y = 1
+      mbbutton.scale.z = 1
+    }
+    if(creativebutton){
+      creativebutton.material.color.setHex(0xff0000)
+      creativebutton.scale.x = 1
+      creativebutton.scale.y = 1
+      creativebutton.scale.z = 1
     }
   }
   return (
