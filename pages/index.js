8d7ae786d7a9c0f69795c73dd7551d37224da428
Dastaan <vaibhav_b200642ce@nitc.ac.in>
2022-01-13 01:48:20 +0530

    cleaned files,ready to work

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,9 @@
 import Head from 'next/head'
-import Nav from '@/components/common/Nav'
 
 export default function Home() {
   return (
     <div >
-      <Nav/>
+     <p>Hello</p>
     </div>
   )
 }
