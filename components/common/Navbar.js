f938d8604a0417cd511e901ab69d81eed2ae01a2
amalpmathews2003 <amalpmathews2003@gmail.com>
2022-01-13 22:32:42 +0530

    navbar changes

--- components/common/Navbar.js
+++ components/common/Navbar.js
@@ -2,8 +2,40 @@ import style from "./navbar.module.css";
 import Image from "next/image";
 import Head from "next/head";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const router = useRouter();
+  const [title, setTitle] = useState("Tathva 21");
+  const [links, setLinks] = useState([
+    { link: "About Us", id: 1, url: "" },
+    { link: "Events", id: 2, url: "" },
+    { link: "Workshops", id: 3, url: "" },
+    { link: "Lecture", id: 4, url: "" },
+    { link: "Shop", id: 5, url: "" },
+  ]);
+  useEffect(() => {
+    if (router.asPath == "/") setTitle("Tathva 21");
+
+    switch (router.asPath) {
+      case "/":
+        setTitle("Tathva 21");
+        break;
+      case "/events":
+        setTitle("EVENTS");
+        break;
+      case "/workshops":
+        setTitle("WORKSHOPS");
+        break;
+      case "/lectures":
+        setTitle("LECTURES");
+        break;
+      case "/profile":
+        setTitle("PROFILE");
+        break;
+    }
+  }, []);
   return (
     <>
       <div className={style.logo}>
@@ -19,12 +51,20 @@ const Navbar = () => {
           className={`${style.navbarg} ${style.no1}`}
           data-augmented-ui
         ></div>
-        <div
-          className={`${style.navbarg} ${style.no2}`}
-          data-augmented-ui
-        ></div>
+        <div className={`${style.navbarg} ${style.no2}`} data-augmented-ui>
+          <ol className={style["navbar-list"]}>
+            <li className={style["navbar-title"]}>{title}</li>
+            {links.map((link) => (
+              <li key={link.id} className={style["navbar-link"]}>
+                {link.link}
+              </li>
+            ))}
+          </ol>
+        </div>
       </div>
-      <div className={style.login} data-augmented-ui></div>
+      <div className={style.login} data-augmented-ui>
+        Login
+      </div>
     </>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   const {user}=useUserContext()
   return (
     <div >
-      Home
+      {/* Home
       <LoginBtn></LoginBtn>
       <LogoutBtn/>
       <Link href="/profile" passHref><button>Profile</button></Link>
@@ -17,7 +17,7 @@ export default function Home() {
       <Link href='/lectures' passHref><button>Lectures</button></Link>
       <br/>
       <Link href='/workshops' passHref><button>Workshops</button></Link>
-      
+       */}
     </div>
   )
 }
