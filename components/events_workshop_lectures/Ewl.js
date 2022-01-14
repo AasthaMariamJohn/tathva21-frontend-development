81e91d8b8a8c33d30f6eb3b9fca7cca215f04d04
ALIAS GEORGE <bps@ALIAS-MacBook-Pro.local>
2022-01-15 00:36:40 +0530

    events page responsive fixed

--- components/events_workshop_lectures/Ewl.js
+++ components/events_workshop_lectures/Ewl.js
@@ -1,5 +1,5 @@
 import style from "./ewl.module.css";
-import {MdAlarm} from 'react-icons/md';
+import { MdAlarm } from "react-icons/md";
 import Vector1 from "./Vector1";
 import Vector2 from "./Vector2";
 import Image from "next/image";
@@ -22,18 +22,31 @@ export default function Event() {
         <Vector2 /> */}
 
         <div className={style.img}>
-          <div className={style.img_desktop} >
-          <Image src="/herodekstop.png" alt="next-image"  width={300} height={200}/>
+          <div className={style.img_desktop}>
+            <Image
+              src="/herodekstop.png"
+              alt="next-image"
+              width={300}
+              height={300}
+              layout="responsive"
+            />
           </div>
           <div className={style.img_mobile}>
-          <Image src="/heromobile.png" alt="next-image"  width={200} height={200}/>
-
+            <Image
+              src="/heromobile.png"
+              alt="next-image"
+              width={1000}
+              height={600}
+              layout="responsive"
+            />
           </div>
         </div>
         <div className={style.content}>
           <div className={style.eventname}>
             <h2 className={style.titles}>EVENT NAME</h2>
-            <MdAlarm/>
+            <div className={style.titles}>
+              <MdAlarm />
+            </div>
             <h3 className={style.titles}>25:03:99</h3>
           </div>
           <div className={style.eventdetails}>
@@ -42,11 +55,10 @@ export default function Event() {
             <h3 className={style.titles}>TERMS&CONDITIONS</h3>
           </div>
           <p className={style.des}>
-            NFT stands for non-fungible token, which basically means
-            that it&apos;s a one-of-a-kind digital asset that belongs to you
-            and you only. The most po24535pular NFTs right now 
-            include artwork and music, but can also include
-            viGDG5353deos and even tweets.
+            NFT stands for non-fungible token, which basically means that
+            it&apos;s a one-of-a-kind digital asset that belongs to you and you
+            only. The most po24535pular NFTs right now include artwork and
+            music, but can also include viGDG5353deos and even tweets.
           </p>
           <div className={style.button} data-augmented-ui>
             REGISTER
