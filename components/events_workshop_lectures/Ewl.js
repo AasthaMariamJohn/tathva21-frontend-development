de6cdc24cb8f8a5e0bc5eb03952acbc4a05b7206
ALIAS GEORGE <bps@ALIAS-MacBook-Pro.local>
2022-01-15 13:23:41 +0530

    events usestate added

--- components/events_workshop_lectures/Ewl.js
+++ components/events_workshop_lectures/Ewl.js
@@ -1,19 +1,8 @@
 import style from "./ewl.module.css";
-import { MdAlarm } from "react-icons/md";
-import Vector1 from "./Vector1";
-import Vector2 from "./Vector2";
-import { Center, Image } from "@chakra-ui/react";
-import { useState } from "react";
 
+import Ewl_component from "../common/ewl_component";
+import { Center } from "@chakra-ui/react";
 export default function Event() {
-  const eventdetails = {
-    info: "Info Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium odio, quia exercitationem, illo nihil repudiandae neque commodi provident, quas aspernatur error repellendus esse optio in impedit ad iure quis dolore. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium odio, quia exercitationem, illo nihil repudiandae neque commodi provident, quas aspernatur error repellendus esse optio in impedit ad iure quis dolore.",
-    rules:
-      "Rules Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium odio, quia exercitationem, illo nihil repudiandae neque commodi provident, quas aspernatur error repellendus esse optio in impedit ad iure quis dolore.",
-    desc: "Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium odio, quia exercitationem, illo nihil repudiandae neque commodi provident, quas aspernatur error repellendus esse optio in impedit ad iure quis dolore.",
-  };
-
-  const [body, setBody] = useState(eventdetails.info);
   return (
     <div className={style.main}>
       <div className={style.list}>
@@ -38,69 +27,7 @@ export default function Event() {
           </ul>
         </div>
       </Center>
-      <div className={style.main2}>
-        {/* <Vector1 />
-        <Vector2 /> */}
-
-        <div className={style.img}>
-          <Center>
-            <div className={style.img_desktop}>
-              <Image src="/images/herodekstop.png" alt="next-image" />
-            </div>
-          </Center>
-          <Center>
-            <div className={style.img_mobile}>
-              <Image src="/images/heromobile.png" alt="next-image" />
-            </div>
-          </Center>
-        </div>
-        <div className={style.content}>
-          <div className={style.eventname}>
-            <h2 className={style.titles}>EVENT NAME</h2>
-            <h3 className={style.titles}>
-              <h3 className={style.titles}>
-                <MdAlarm />
-              </h3>
-              25:03:99
-            </h3>
-          </div>
-          <div className={style.eventdetails}>
-            <h3 className={style.titles}>
-              <button
-                onClick={() => {
-                  setBody(eventdetails.info);
-                }}
-              >
-                INFO
-              </button>
-            </h3>
-            <h3 className={style.titles}>
-              <button
-                onClick={() => {
-                  setBody(eventdetails.rules);
-                }}
-              >
-                RULES
-              </button>
-            </h3>
-            <h3 className={style.titles}>
-              <button
-                onClick={() => {
-                  setBody(eventdetails.desc);
-                }}
-              >
-                TERMS&CONDITIONS
-              </button>
-            </h3>
-          </div>
-          <p className={style.des}>{body}</p>
-          <Center>
-            <div className={style.button} data-augmented-ui>
-              REGISTER
-            </div>
-          </Center>
-        </div>
-      </div>
+      <Ewl_component />
     </div>
   );
 }
