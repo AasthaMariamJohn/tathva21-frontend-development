4f1f0e5675a4dddae0a93620982c2d3b59de3858
ALIAS GEORGE <bps@ALIAS-MacBook-Pro.local>
2022-01-16 22:32:26 +0530

    update navbar

--- components/common/navbarsvg.js
+++ components/common/navbarsvg.js
@@ -1,90 +1,34 @@
-function Navbarsvg1() {
-  return (
-    <svg
-      width="877"
-      height="34"
-      viewBox="0 0 877 34"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <g filter="url(#filter0_d_535_1113)">
-        <path
-          d="M4 25H228.314C235.906 25 243.399 23.2711 250.223 19.9444L278.715 6.0556C285.539 2.72891 293.032 1 300.624 1H873"
-          stroke="url(#paint0_linear_535_1113)"
-          strokeOpacity="0.58"
-          strokeWidth="2"
-          shapeRendering="crispEdges"
-        />
-      </g>
-      <defs>
-        <filter
-          id="filter0_d_535_1113"
-          x="0"
-          y="0"
-          width="877"
-          height="34"
-          filterUnits="userSpaceOnUse"
-          colorInterpolationFilters="sRGB"
-        >
-          <feFlood floodOpacity="0" result="BackgroundImageFix" />
-          <feColorMatrix
-            in="SourceAlpha"
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
-            result="hardAlpha"
-          />
-          <feOffset dy="4" />
-          <feGaussianBlur stdDeviation="2" />
-          <feComposite in2="hardAlpha" operator="out" />
-          <feColorMatrix
-            type="matrix"
-            values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
-          />
-          <feBlend
-            mode="normal"
-            in2="BackgroundImageFix"
-            result="effect1_dropShadow_535_1113"
-          />
-          <feBlend
-            mode="normal"
-            in="SourceGraphic"
-            in2="effect1_dropShadow_535_1113"
-            result="shape"
-          />
-        </filter>
-        <linearGradient
-          id="paint0_linear_535_1113"
-          x1="-443.58"
-          y1="25"
-          x2="1554.89"
-          y2="30.8211"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="white" />
-          <stop offset="0.0001" stopColor="#1FAEFF" />
-          <stop offset="0.9999" stopColor="#00A3FF" />
-          <stop offset="1" stopColor="white" />
-        </linearGradient>
-      </defs>
-    </svg>
-  );
-}
+import { useEffect, useState } from "react";
+export default function Navbarsvg() {
+  const [WindowDimensions, setWindowDimensions] = useState(1200);
 
-function Navbarsvg2() {
+  useEffect(() => {
+    const getWindowDimensions = () => {
+      const width = window.screen.width;
+      return width;
+    };
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <svg
-      width="120"
-      height="44"
-      viewBox="0 0 490 34"
+      width={WindowDimensions * 0.72}
+      height="34"
+      viewBox={"0 0 " + WindowDimensions * 0.72 + " 34"}
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
       <g filter="url(#filter0_d_535_1114)">
         <path
-          d="M4 25H143.991C155.197 25 166.077 21.2361 174.887 14.312L178.226 11.688C187.036 4.76395 197.917 1 209.122 1H485.5"
+          d="M4 25H198.42C206.903 25 215.247 22.8417 222.666 18.7281L243.328 7.27186C250.747 3.15832 259.09 1 267.573 1H1305"
           stroke="url(#paint0_linear_535_1114)"
           strokeOpacity="0.58"
-          strokeWidth="5"
+          strokeWidth="2"
           shapeRendering="crispEdges"
         />
       </g>
@@ -93,7 +37,7 @@ function Navbarsvg2() {
           id="filter0_d_535_1114"
           x="0"
           y="0"
-          width="489.5"
+          width="1309"
           height="34"
           filterUnits="userSpaceOnUse"
           colorInterpolationFilters="sRGB"
@@ -126,10 +70,10 @@ function Navbarsvg2() {
         </filter>
         <linearGradient
           id="paint0_linear_535_1114"
-          x1="-262.613"
-          y1="25.0004"
-          x2="976.949"
-          y2="27.2399"
+          x1="-389.5"
+          y1="25"
+          x2="1367.5"
+          y2="29.4994"
           gradientUnits="userSpaceOnUse"
         >
           <stop stopColor="white" />
@@ -141,5 +85,3 @@ function Navbarsvg2() {
     </svg>
   );
 }
-
-export { Navbarsvg1, Navbarsvg2 };
