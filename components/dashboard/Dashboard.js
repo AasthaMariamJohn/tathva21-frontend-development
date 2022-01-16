1277e4badb05ca49c42aad30c91ca3f442e494fd
amalpmathews2003 <amalpmathews2003@gmail.com>
2022-01-16 13:41:09 +0530

    overlay added

--- components/dashboard/Dashboard.js
+++ components/dashboard/Dashboard.js
@@ -1,23 +1,18 @@
-import style from "./db.module.css"
+import style from "./db.module.css";
 import Section2 from "./section2";
-import EventComponent from "./EventComponent"
+import EventComponent from "./EventComponent";
 import Team from "./Team";
 
-
-    
-
-
-export default function Dashboard(){
-    return(
-        <div className={style.main}>
-
-           <div className={style.grid}>
-               <div className={style.section1}>
-                   <EventComponent/>
-                   <Team/>
-               </div>
-           <Section2/>
-           </div> 
-
+export default function Dashboard() {
+  return (
+    <div className={style.main}>
+      <div className={style.grid}>
+        <div className={style.section1}>
+          <EventComponent />
+          <Team />
         </div>
-    )}
+        <Section2 />
+      </div>
+    </div>
+  );
+}
--- components/dashboard/Dropbox.js
+++ components/dashboard/Dropbox.js
@@ -3,9 +3,10 @@ import style from "./db.module.css"
 export default function Dropbox(){
     return(
         <div className={style.dropbox}>
-
-                <span class="drop-zone__prompt">Drop file here or click to upload</span>
+            
+            <span >Drop file here or click to upload</span>
             <input type="file" name="myFile" className={style.dropZoneInput}/>
         </div>
     )
 }
+//className={style["drop-zone__prompt"]}
