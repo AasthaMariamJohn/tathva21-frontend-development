4cb946bb6c7ed1137b4617ad624295e2b8f8286a
joelrobin18 <joelrobin1818@gmail.com>
2022-01-17 10:19:51 +0530

    Team Functionality added

---
+++ components/team/Design.js
@@ -0,0 +1,56 @@
+import styles from '../styles/Home.module.css'
+export default function Prc() {
+    return (
+        <div className={styles.teamssection}>
+
+            <div className={styles.teamsection}>
+                <div className={styles.teamimg}></div>
+                <div className={styles.teaminfo}>
+                    <h4>Name</h4>
+                    <h4>Design</h4>
+                </div>
+            </div>
+
+            <div className={styles.teamsection}>
+                <div className={styles.teamimg}></div>
+                <div className={styles.teaminfo}>
+                    <h4>Name</h4>
+                    <h4>Design</h4>
+                </div>
+            </div>
+
+            <div className={styles.teamsection}>
+                <div className={styles.teamimg}></div>
+                <div className={styles.teaminfo}>
+                    <h4>Name</h4>
+                    <h4>Design</h4>
+                </div>
+            </div>
+
+            <div className={styles.teamsection}>
+                <div className={styles.teamimg}></div>
+                <div className={styles.teaminfo}>
+                    <h4>Name</h4>
+                    <h4>Design</h4>
+                </div>
+            </div >
+
+            <div className={styles.teamsection}>
+                <div className={styles.teamimg}></div>
+                <div className={styles.teaminfo}>
+                    <h4>Name</h4>
+                    <h4>Design</h4>
+                </div>
+            </div >
+
+            <div className={styles.teamsection}>
+                <div className={styles.teamimg}></div>
+                <div className={styles.teaminfo}>
+                    <h4>Name</h4>
+                    <h4>Design</h4>
+                </div>
+            </div >
+
+        </div >
+    )
+}
--- components/team/team.js
+++ components/team/team.js
@@ -1,69 +1,91 @@
-import styles from "team.module.css"
-export default function Test() {
+import Head from 'next/head'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import styles from '../styles/Home.module.css'
+import PRC from './Prc'
+import Tech from './Tech'
+import Design from './Design'
+import Market from './Market'
+import Tesla from './Tesla'
+import Google from './Google'
+
+
+export default function Home() {
+
+    const openprc = () => {
+        setPrc(true);
+        setTech(false);
+        setDesign(false);
+        setMarket(false);
+        setTesla(false);
+        setGoogle(false);
+    }
+    const opentech = () => {
+        setPrc(false);
+        setTech(true);
+        setDesign(false);
+        setMarket(false);
+        setTesla(false);
+        setGoogle(false);
+    }
+    const opendesign = () => {
+        setDesign(true);
+        setPrc(false);
+        setTech(false);
+        setMarket(false);
+        setTesla(false);
+        setGoogle(false);
+    }
+    const openmarket = () => {
+        setPrc(false);
+        setTech(false);
+        setDesign(false);
+        setMarket(true);
+        setTesla(false);
+        setGoogle(false);
+    }
+    const opentesla = () => {
+        setPrc(false);
+        setTech(false);
+        setDesign(false);
+        setMarket(false);
+        setTesla(true);
+        setGoogle(false);
+    }
+    const opengoogle = () => {
+        setPrc(false);
+        setTech(false);
+        setDesign(false);
+        setMarket(false);
+        setTesla(false);
+        setGoogle(true);
+    }
+
+    const [prc, setPrc] = useState(true);
+    const [tech, setTech] = useState(false);
+    const [design, setDesign] = useState(false);
+    const [market, setMarket] = useState(false);
+    const [tesla, setTesla] = useState(false);
+    const [google, setGoogle] = useState(false);
+
     return (
         <div className={styles.container}>
             <div className={styles.teamdesktop}>
 
                 <div className={styles.teamheading}>
-                    <li>PRC</li>
-                    <li>Tesla</li>
-                    <li>Design</li>
-                    <li>Marketing</li>
-                    <li>Tesla</li>
-                    <li>Google</li>
+                    <li onClick={openprc}>PRC</li>
+                    <li onClick={opentech}>Tech</li>
+                    <li onClick={opendesign}>Design</li>
+                    <li onClick={openmarket}>Marketing</li>
+                    <li onClick={opentesla}>Tesla</li>
+                    <li onClick={opengoogle}>Google</li>
                 </div>
-
-                <div className={styles.teamssection}>
-
-                    <div className={styles.teamsection}>
-                        <div className={styles.teamimg}></div>
-                        <div className={styles.teaminfo}>
-                            <h4>Name</h4>
-                            <h4>Design</h4>
-                        </div>
-                    </div>
-
-                    <div className={styles.teamsection}>
-                        <div className={styles.teamimg}></div>
-                        <div className={styles.teaminfo}>
-                            <h4>Name</h4>
-                            <h4>Design</h4>
-                        </div>
-                    </div>
-
-                    <div className={styles.teamsection}>
-                        <div className={styles.teamimg}></div>
-                        <div className={styles.teaminfo}>
-                            <h4>Name</h4>
-                            <h4>Design</h4>
-                        </div>
-                    </div>
-
-                    <div className={styles.teamsection}>
-                        <div className={styles.teamimg}></div>
-                        <div className={styles.teaminfo}>
-                            <h4>Name</h4>
-                            <h4>Design</h4>
-                        </div>
-                    </div >
-
-                    <div className={styles.teamsection}>
-                        <div className={styles.teamimg}></div>
-                        <div className={styles.teaminfo}>
-                            <h4>Name</h4>
-                            <h4>Design</h4>
-                        </div>
-                    </div >
-
-                    <div className={styles.teamsection}>
-                        <div className={styles.teamimg}></div>
-                        <div className={styles.teaminfo}>
-                            <h4>Name</h4>
-                            <h4>Design</h4>
-                        </div>
-                    </div >
-
-                </div >
+                {prc && < PRC />}
+                {tech && <Tech />}
+                {design && <Design />}
+                {market && <Market />}
+                {tesla && <Tesla />}
+                {google && <Google />}
             </div >
         </div >
     )
