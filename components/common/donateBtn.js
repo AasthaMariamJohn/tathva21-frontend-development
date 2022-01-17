51313a4641158b002af31de393cbd70957632e9a
amalpmathews2003 <amalpmathews2003@gmail.com>
2022-01-17 16:03:02 +0530

    donate btn css changed

--- components/common/donateBtn.js
+++ components/common/donateBtn.js
@@ -1,7 +1,7 @@
 import styles from './donateBtn.module.css'
 export default function DonateBtn(){
       return(
-            <div className={styles.donate} data-augmented-ui>
+            <div className={styles.donate}>
             Donate
           </div>
       )
