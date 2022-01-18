870a05e2647085ce3ff964e3a36759e711191e64
amalpmathews2003 <amalpmathews2003@gmail.com>
2022-01-18 09:15:39 +0530

    try to change css of ewl component

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,17 @@ import DonateBtn from '@/components/common/donateBtn'
 import Head from 'next/head'
 
 import BasicUsage from '@/components/common/eventModal'
+import { useEffect } from 'react/cjs/react.development'
+import { Login } from '@/lib/user/login'
+import { Router, useRouter } from 'next/router'
 export default function Home() {
   
- 
-  // const {user}=useUserContext()
+  const {user}=useUserContext()
+  const router=useRouter()
+  useEffect(()=>{
+    if(user && user.tathvaId && !user.name)
+      Login(router)
+  },[user])
   return (
     <div >
        <Head>
