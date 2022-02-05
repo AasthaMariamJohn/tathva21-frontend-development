166ffc0591147e0aef647fb1067242f335428717
Drake <rockydrake987@gmail.com>
2022-02-05 17:00:56 +0530

    update on sponsor

--- components/sponsors/sponsors.js
+++ components/sponsors/sponsors.js
@@ -7,31 +7,129 @@ export default function Sponsers() {
         <h1>Sponsors</h1>
       </div>
       <div className={styles["sponsor"]}>
-        <div className={styles["sponsorImg"]}>
+          <p>Education Partner </p>
+          <div className={styles["group2"]}>
+          <a href="https://entri.app/courses/gate-coaching/" target=" "><Image
+            src={"/sponsor/Logo.png"}
+            alt="next-image"
+            width={"350px"}
+            height={"100px"}
+            link={"https://entri.app/courses/gate-coaching/"}
+          />
+          <br /> 
+          </a>
+          </div>
           <p>Coding Partner</p>
+          <div className={styles["group2"]}>
+          <a href="https://www.codingninjas.com/" target=" ">
           <Image
             src={"/cn.png"}
             alt="next-image"
-            width={"400vw"}
-            height={"200vh"}
+            width={"350px"}
+            height={"100px"}
+          />
+          </a>
+          <br />
+          </div>
+          <p>Workshop Sponsor</p>
+          <div className={styles["group2"]}>
+          <a href="https://entri.app/courses/gate-coaching/" target=" ">
+          <Image
+            src={"/sponsor/techmaaghilogo.png"}
+            alt="next-image"
+            width={"350px"}
+            height={"100px"}
+          />
+          </a>
+          <br />
+          </div>
+          <p>Workshop Partners </p>
+          <div className={styles["group1"]}>
+          <a href="https://www.linkedin.com/company/dertlab/?originalSubdomain=in" target=" ">
+          <Image
+            src={"/sponsor/PACELABSilver.png"}
+            alt="next-image"
+            width={"350px"}
+            height={"100px"}
+          />
+          </a>
+          <br />
+          <a href="https://www.pacelab.tech/" target=" ">
+          <Image
+            src={"/sponsor/PACELABSilver.png"}
+            alt="next-image"
+            width={"350px"}
+            height={"100px"}
+          />
+          </a>
+          <br />
+          <a href="http://www.ht-lab.com/" target=" ">
+          <Image
+            src={"/sponsor/htindia-logo12.png"}
+            alt="next-image"
+            width={"350px"}
+            height={"100px"}
+          />
+          </a>
+          <br />
+          </div>
+          <p>Industrial Training and Project Partner</p>
+          <div className={styles["group2"]}>
+          <a href="https://entri.app/courses/gate-coaching/" target=" ">
+          <Image
+            src={"/cn.png"}
+            alt="next-image"
+            width={"350px"}
+            height={"100px"}
           />
           <br />
-          <p>Previous Sponsors</p>
-          <div className={styles["sponsor-images"]}>
-            <Image
-              src={"/images/sponsor1.jpg"}
-              alt="next-image"
-              width={"2000vw"}
-              height={"1000vh"}
-            />
-            <Image
-              src={"/images/sponsor2.jpg"}
-              alt="next-image"
-              width={"2000vw"}
-              height={"1000vh"}
-            />
+          </a>
           </div>
-        </div>
+          <p>Platform Partners  </p>
+          <div className={styles["group1"]}>
+          <a href="https://www.gather.town/" target=" ">
+          <Image
+            src={"/sponsor/phpe9mLN5.png"}
+            alt="next-image"
+            width={"350px"}
+            height={"100px"}
+          />
+          </a>
+          <br />
+          <a href=" https://www.chess.com/" target=" ">
+          <Image
+            src={"/sponsor/phpe9mLN5.png"}
+            alt="next-image"
+            width={"350px"}
+            height={"100px"}
+          />
+          </a>
+          <br />
+          </div>
+          <p>Social Media Partner </p>
+          <div className={styles["group2"]}>
+          <a href="https://pinklungi.com/" target=" ">
+          <Image
+            src={"/cn.png"}
+            alt="next-image"
+            width={"350px"}
+            height={"100px"}
+          />
+          </a>
+          <br />
+          </div>
+          <p>Beverage Partner  </p>
+          <div className={styles["group2"]}>
+          <a href="https://entri.app/courses/gate-coaching/" target=" ">
+          <Image
+            src={"/cn.png"}
+            alt="next-image"
+            width={"350px"}
+            height={"100px"}
+          />
+          </a>
+          <br />
+          </div>
       </div>
     </div>
   );
